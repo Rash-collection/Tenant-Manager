d49f05f16d446640274aa25232e1584cc3ae4128
Rash-collection <195858877+Rash-collection@users.noreply.github.com>
2026-03-06 02:05:20 +0200

    first commit

---
+++ src/main/java/launch/Towers.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package launch;
+
+/**
+ *
+ * @author rash4
+ */
+public class Towers {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
