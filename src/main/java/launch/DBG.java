8092393cdcb029ebd8fa6446d2c80152b9e57f22
Rash-collection <195858877+Rash-collection@users.noreply.github.com>
2026-03-14 20:08:22 +0200

    The 'debug-console' with the initial commands.

---
+++ src/main/java/launch/DBG.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package launch;
+
+import coms.Command;
+import coms.CommandTree;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author rash4
+ */
+public class DBG extends coms.Console{
+    DBG(){super();}
+    
+    @Override public void initialize(){
+        super.initialize(true);
+        super.setCommands(new CMR(this).createCommands());
+    }
+    class CMR extends coms.Commanding{
+        CMR(DBG dbg){super(dbg);}
+        CMR createCommands(){
+            final var help  = new CommandTree();
+            final var exit  = new Command(arg->{System.exit(0);  return true;});
+            final var clear = new Command(arg->{DBG.this.clear();return true;});
+            help.setDirectCom(  new Command(super::getHelp));
+            help.setCom("full", new Command(super::fullHelpList));
+            this    .addCommand(help,  "help")
+                    .addCommand(clear, "clear")
+                    .addCommand(exit,  "exit")
+                    ;
+            return this;
+        }
+    }
+}
